b9d2b6b4d077451f08fe56ab9758721ce5f20cf8
Doug K <doug.kerzner@gmail.com>
2021-01-14 19:29:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,7 +25,7 @@ end
 def return_string_array(array)
   counter = 0 
   newarray = []
-  while counter <= array.length do
+  while counter < array.length do
     newarray << array[counter].to_s 
     counter += 1 
 end 
